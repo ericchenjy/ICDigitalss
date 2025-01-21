251cc574207a31dd42913166b34681b5dee3b57e
Eric Chen <ericchen@Erics-MacBook-Pro-2.local>
2025-01-21 04:25:12 -0500

    building the menu bar

---
+++ src/app/menu-bar/menu-bar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.scss']
+})
+export class MenuBarComponent {
+
+  constructor(private router: Router){
+
+  }
+
+  pageRouter(path: string): void{
+    this.router.navigate([path])
+  }
+
+
+}
+
+  
+
