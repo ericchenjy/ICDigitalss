d5df953b7bb481fc05fbf8c50b3a566f89a91789
Eric Chen <ericchen@MacBook-Pro.local>
2025-02-06 19:52:29 -0500

    updataing

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-menu-bar',
-  templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.scss']
+    selector: 'app-menu-bar',
+    templateUrl: './menu-bar.component.html',
+    styleUrls: ['./menu-bar.component.scss'],
+    standalone: false
 })
 export class MenuBarComponent {
 
